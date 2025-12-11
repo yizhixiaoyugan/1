1f679cc4e2e732f02d15805ef51f3844a04c083a
shijiayi390 <shijiayi390@gmail.com>
2025-12-11 16:41:56 +0800

    update config

--- apps/frontend/vite.config.ts
+++ apps/frontend/vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // 从环境变量获取仓库名，用于 GitHub Pages 部署
 // 你需要把 'ci-cd-demo' 改成你的实际仓库名
-const base = process.env.GITHUB_ACTIONS ? '/test/' : '/'
+const base = process.env.GITHUB_ACTIONS ? '/1/' : '/'
 
 export default defineConfig({
   base,
